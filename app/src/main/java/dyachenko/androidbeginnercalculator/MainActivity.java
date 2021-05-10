04822f07f797d358fbabf0ae57578760c074c005
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-05-10 23:16:19 +0300

    Задание 1: добавлены настройки (вызов через меню), изменение темы программно Задание 2: больше ничего не добавлено, все уже реализовано раньше

--- app/src/main/java/dyachenko/androidbeginnercalculator/MainActivity.java
+++ app/src/main/java/dyachenko/androidbeginnercalculator/MainActivity.java
@@ -1,30 +1,102 @@
 package dyachenko.androidbeginnercalculator;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final static String KEY_CALCULATOR_DATA = "EXTRA_CALCULATOR_DATA";
+    public final static String KEY_THEME = "EXTRA_THEME";
+    public final static int APP_DAY_THEME = 0;
+    public final static int APP_NIGHT_THEME = 1;
+    public final static int DEFAULT_DAY_THEME = 2;
+    public final static int DEFAULT_NIGHT_THEME = 3;
+    private final static int SETTINGS_REQUEST_CODE = 1;
     private TextView leftOperandTextView;
     private TextView rightOperandTextView;
     private TextView errorTextView;
     private Calculator calculator;
+    private int theme = APP_DAY_THEME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            theme = savedInstanceState.getInt(KEY_THEME);
+        }
+        setAppTheme();
+
         setContentView(R.layout.activity_main);
 
         calculator = new Calculator();
         initViews();
         updateViews();
+    }
+
+    private void setAppTheme() {
+        switch (theme) {
+            case APP_NIGHT_THEME:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                setTheme(R.style.Theme_Calculator);
+                break;
+            case DEFAULT_DAY_THEME:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                setTheme(R.style.Theme_AndroidBeginnerCalculator);
+                break;
+            case DEFAULT_NIGHT_THEME:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                setTheme(R.style.Theme_AndroidBeginnerCalculator);
+                break;
+            default:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                setTheme(R.style.Theme_Calculator);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            intent.putExtra(KEY_THEME, theme);
+            startActivityForResult(intent, SETTINGS_REQUEST_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != SETTINGS_REQUEST_CODE) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Bundle extras = data.getExtras();
+                theme = extras.getInt(KEY_THEME);
+                recreate();
+            }
+        }
     }
 
     private void initViews() {
@@ -54,6 +126,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelable(KEY_CALCULATOR_DATA, calculator.createCalculatorData());
+        outState.putInt(KEY_THEME, theme);
     }
 
     @Override
